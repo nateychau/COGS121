d6ed3fa5d17381fb462854bc55dc2f3f18d90255
jfong02 <jfong02@users.noreply.github.com>
2019-05-19 19:14:53 -0700

    Geocoder working now
    Also moved the entire google maps api key to javascript

--- static_files/js/instructor_location.js
+++ static_files/js/instructor_location.js
@@ -1,80 +1,88 @@
-var geocoder;
-var map;
-var infoWindow;
-var marker = new google.maps.Marker({
-    animation: google.maps.Animation.DROP,
-    map: map,
-    position: null
-});
+var scriptkey = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCWsb3UJbBCnDwp2vM8cYsEKfBG2jtvyoY";
 
-var addressData = {
-    address: "9500 Gilman Dr.",
-};
+$.getScript(scriptkey, function() {
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: {lat: -34.397, lng: 150.644}
-    });
-    getcurrentLocation();
-
-    // geocoder = new google.maps.Geocoder();
-    // codeAddress(geocoder, map);
-}
-
-function getcurrentLocation() {
-    // infoWindow = new google.maps.InfoWindow;
-
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        };
-
-        // infoWindow.setPosition(pos);
-        // infoWindow.setContent('Current Location');
-        // infoWindow.open(map);
-        map.setCenter(pos);
-        }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-}
-
-function codeAddress(geocoder, map) {
-    geocoder.geocode({'address': addressData.address}, function(results, status) {
-        if (status === 'OK') {
-            marker.setPosition(results[0].geometry.location);
-            // map.setCenter(results[0].geometry.location);
-            // map.setZoom(15);
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-        }
-    });
-}
-
-$('#addAddress').click(() =>{
-    console.log('Adding address to map');
-    addressData.address = document.getElementById("AddressSearchBar").value;
-    console.log(addressData);
     // Creates a new geocoder object
-    geocoder = new google.maps.Geocoder();
-    // calls codeAddress method (above) to plot a new marker
-    codeAddress(geocoder, map);
-    sleep(10000);
-    // $('#map').show();
-});
+    var geocoder;
+    var map;
+    var infoWindow;
+    var marker;
+    initMap();
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > milliseconds){
-            break;
+    var addressData = {
+        address: "7655 Palmilla Dr.",
+    };
+
+    function initMap() {
+        geocoder= new google.maps.Geocoder();
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: {lat: -34.397, lng: 150.644}
+        });
+        marker = new google.maps.Marker({
+            animation: google.maps.Animation.DROP,
+            map: map,
+            position: null,
+        });
+        getcurrentLocation();
+
+        // codeAddress(geocoder, map);
+    }
+
+    function getcurrentLocation() {
+        // infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            // infoWindow.setPosition(pos);
+            // infoWindow.setContent('Current Location');
+            // infoWindow.open(map);
+            map.setCenter(pos);
+            }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
         }
     }
-}
+
+    function codeAddress(geocoder, map) {
+        console.log("TEST");
+        geocoder.geocode({'address': addressData.address}, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                marker.setPosition(results[0].geometry.location);
+                map.setCenter(results[0].geometry.location);
+                map.setZoom(15);
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
+        });
+    }
+
+    $('#addAddress').click(() =>{
+        console.log('Adding address to map');
+        addressData.address = document.getElementById("AddressSearchBar").value;
+        console.log(addressData);
+        // calls codeAddress method (above) to plot a new marker
+        codeAddress(geocoder, map);
+        // sleep(10000);
+        // $('#map').show();
+    });
+
+    // Custom function to make the computer wait...
+    function sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds){
+                break;
+            }
+        }
+    }
+});
