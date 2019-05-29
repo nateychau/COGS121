d312c332b88d5659c9b999701b563b37ae87b43d
jfong02 <jkf002@ucsd.edu>
2019-05-29 05:37:08 -0700

    Revamped instructor_prof completely, updated milestone5
    -also slight indentation fix for instructor_start just for clean code viewing :)

--- static_files/js/instructor_start.js
+++ static_files/js/instructor_start.js
@@ -5,84 +5,84 @@ $(document).ready(() =>{
     $('#instructorStartSubmit').click(()=>{
         const ref = database.ref();
         ref.once('value', function(snapshot){
-		const firstname = $('#firstNameInput').val();
-		const lastname = $('#lastNameInput').val();
-		const username = $('#usernameInput').val();
-        const password = $('#passwordInput').val();
-        const passwordVerify = $('#passwordVerifyInput').val();
-		const email = $('#emailInput').val();
-		const phone = $('#phoneNumberInput').val();
-		
-		var inval = false;	// invalid input somewhere
-		if (firstname == null || firstname == "") {
-			$('#firstNameInput').addClass("invalidInput");
-			inval = true;
-		}
-		if (lastname == null || lastname == "") {
-			$('#lastNameInput').addClass("invalidInput");
-			inval = true;
-		}
-		if (username == null || username == "") {
-			$('#usernameInput').addClass("invalidInput");
-			inval = true;
-        }
-        if(snapshot.child('users/'+username).exists()){
-            inval = true;
-            $('#usernameInput').addClass("invalidInput");
-            console.log(username + ' already exists');
-            alert(username +' already exists')
-        }
-        if (password == null || password == "") {
-			$('#passwordInput').addClass("invalidInput");
-			inval = true;
-        }
-        if (passwordVerify == null || passwordVerify == "") {
-			$('#passwordVerifyInput').addClass("invalidInput");
-			inval = true;
-        }
-        // Check passwords match
-        if (!verify()) {
-			inval = true;
-        }
-		if (email == null || email == "") {
-			$('#emailInput').addClass("invalidInput");
-			inval = true;
-		}
-		if (phone == null || phone == "") {
-			$('#phoneNumberInput').addClass("invalidInput");
-			inval = true;
-        }
-        console.log("invalid input:" + inval);
-		if (!inval) {
-			database.ref('users/'+username).set({
-				lastname: lastname,
-				firstname: firstname,
-				password: password,
-				email: email,
-				phone: phone,
-			}, (error)=> {
-                if(error) {
-                    console.log("Error:" +  error);
-                    // write failed
-                    alert("An error occured when adding " + username + " to the database: " + error);
-                }
-                else {
-                    // data saved successfully
-                    snackbarActivate(username + " added to the database");
-                    window.location.href = "instructor_detail.html";
-                }
-            });
-            // Starts a "session" to remember the person in next few pages (idk if needed)
-            // database.ref('sessions/'+username).set({
-            //     name: name,
-            // });
+            const firstname = $('#firstNameInput').val();
+            const lastname = $('#lastNameInput').val();
+            const username = $('#usernameInput').val();
+            const password = $('#passwordInput').val();
+            const passwordVerify = $('#passwordVerifyInput').val();
+            const email = $('#emailInput').val();
+            const phone = $('#phoneNumberInput').val();
+            
+            var inval = false;	// invalid input somewhere
+            if (firstname == null || firstname == "") {
+                $('#firstNameInput').addClass("invalidInput");
+                inval = true;
+            }
+            if (lastname == null || lastname == "") {
+                $('#lastNameInput').addClass("invalidInput");
+                inval = true;
+            }
+            if (username == null || username == "") {
+                $('#usernameInput').addClass("invalidInput");
+                inval = true;
+            }
+            if(snapshot.child('users/'+username).exists()){
+                inval = true;
+                $('#usernameInput').addClass("invalidInput");
+                console.log(username + ' already exists');
+                alert(username +' already exists')
+            }
+            if (password == null || password == "") {
+                $('#passwordInput').addClass("invalidInput");
+                inval = true;
+            }
+            if (passwordVerify == null || passwordVerify == "") {
+                $('#passwordVerifyInput').addClass("invalidInput");
+                inval = true;
+            }
+            // Check passwords match
+            if (!verify()) {
+                inval = true;
+            }
+            if (email == null || email == "") {
+                $('#emailInput').addClass("invalidInput");
+                inval = true;
+            }
+            if (phone == null || phone == "") {
+                $('#phoneNumberInput').addClass("invalidInput");
+                inval = true;
+            }
+            console.log("invalid input:" + inval);
+            if (!inval) {
+                database.ref('users/'+username).set({
+                    lastname: lastname,
+                    firstname: firstname,
+                    password: password,
+                    email: email,
+                    phone: phone,
+                }, (error)=> {
+                    if(error) {
+                        console.log("Error:" +  error);
+                        // write failed
+                        alert("An error occured when adding " + username + " to the database: " + error);
+                    }
+                    else {
+                        // data saved successfully
+                        snackbarActivate(username + " added to the database");
+                        window.location.href = "instructor_detail.html";
+                    }
+                });
+                // Starts a "session" to remember the person in next few pages (idk if needed)
+                // database.ref('sessions/'+username).set({
+                //     name: name,
+                // });
 
-            // Save the name
-            localStorage.setItem('keyName',username);
-        }
-        
+                // Save the name
+                localStorage.setItem('keyName',username);
+            }
+            
 
-    });
+        });
     });
 
 });
