6ed28b6533564649cf2022449a96f4e827bbae42
Nathan Chau <nychau@ucsd.edu>
2019-06-01 12:14:11 -0700

    testing with results.html

--- static_files/js/results.js
+++ static_files/js/results.js
@@ -25,17 +25,63 @@ $(document).ready(() =>{
               if(searchName == "" && searchNear == ""){ 
               $('#query').html('All users');
               $('#status').append(
-                  '<br> Name: <a id ="'+username+'ProfLink" href="result_prof.html">'+firstname+ ' '+lastname+ '</a><br>'+
-                  'User ID: '+username + '<br>' +
-                  'Email: '+email+ '<br>'+
-                  'Phone: '+phone +'<br>'+
-                  'About: '+about +'<br>'+
-                  'Experience: '+experience +'<br>'+
-                  'Availability: '+availability +'<br>'+
-                  'Price: '+price+'<br>' +
-                  'Profile Pic:' +
-                  '<img class="profilepic" src='+prof+"/>  <br>"
-                );
+                  // '<br> Name: <a id ="'+username+'ProfLink" href="result_prof.html">'+firstname+ ' '+lastname+ '</a><br>'+
+                  // 'User ID: '+username + '<br>' +
+                  // 'Email: '+email+ '<br>'+
+                  // 'Phone: '+phone +'<br>'+
+                  // 'About: '+about +'<br>'+
+                  // 'Experience: '+experience +'<br>'+
+                  // 'Availability: '+availability +'<br>'+
+                  // 'Price: '+price+'<br>' +
+                  // 'Profile Pic:' +
+                  // '<img class="profilepic" src='+prof+"/>  <br>"+
+                  // '<br></br><br></br>'+
+                  // '<table class = "table">'+
+                  //   '<thead>'+
+                  //     '<tr>'+
+                  //       '<th scope = "col"></th>'+
+                  //       '<th scope = "col"><a id ="'+username+'ProfLink" href="result_prof.html">'+firstname+ ' '+lastname+ '</a></th>'+
+                  //     '</tr>'+
+                  //   '</thead>'+
+                  //   '<tbody>'+
+                  //     '<tr>'+
+                  //       '<th scope = "row"></th>'+
+                  //       '<td><img class="profilepic" src='+prof+'/></td>'+
+                  //     '</tr>'+
+                  //     '<tr>'+
+                  //       '<th scope = "row">Email:</th>'+
+                  //       '<td>'+email+'</td>'+
+                  //     '</tr>'+
+                  //     '<tr>'+
+                  //       '<th scope = "row">Phone</th>'+
+                  //       '<td>'+phone+'</td>'+
+                  //     '</tr>'+
+                  //     '<tr>'+
+                  //       '<th scope = "row">About</th>'+
+                  //       '<td>'+about+'</td>'+
+                  //     '</tr>'+
+                  //     '<tr>'+
+                  //       '<th scope = "row">Experience</th>'+
+                  //       '<td>'+experience+'</td>'+
+                  //     '</tr>'+
+                  //     '<tr>'+
+                  //       '<th scope = "row">Availability</th>'+
+                  //       '<td>'+availability+'</td>'+
+                  //     '</tr>'+
+                  //     '<tr>'+
+                  //       '<th scope = "row">Price</th>'+
+                  //       '<td>'+price+'</td>'+
+                  //     '</tr>'+
+
+                  //   '</tbody>'+
+                  //   '</table>'
+
+               
+               
+               
+               
+               
+                  );
                 console.log($('#'+username+'ProfLink').html());
                 console.log(document.getElementById(username+'ProfLink').id);
                 $('#'+username+'ProfLink').click(()=>{
